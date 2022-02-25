c1069685aa995c089c21b4613b3d17759facff58
HRSHDG8 <hrshdg8@gmail.com>
2022-02-25 14:11:46 -0800

    remove logs

--- src/worker-fetch.ts
+++ src/worker-fetch.ts
@@ -1,12 +1,13 @@
 import {Worker} from 'worker_threads'
 // @ts-ignore
 import {RequestInfo, RequestInit} from 'node-fetch'
+import path from "path";
 
 export const fetch = <T>(url: RequestInfo, init?: RequestInit) : Promise<T> => {
     return new Promise<T>((resolve, reject) => {
 
         // import workerExample.js script..
-        const worker = new Worker(__dirname+`/worker/worker.js`, {workerData: {url, init}, eval: false});
+        const worker = new Worker(path.join(__dirname,'worker','worker.js'), {workerData: {url, init}, eval: false});
         worker.on('message', resolve);
         worker.on('error', reject);
         worker.on('exit', (code) => {
--- src/worker/worker.js
+++ src/worker/worker.js
@@ -1,6 +1,5 @@
 const {workerData, parentPort, threadId} = require('worker_threads')
 const {url, init} = workerData
-console.log('process :: ', threadId)
 import('node-fetch')
     .then((fetch) => {
         fetch.default(url, init)
